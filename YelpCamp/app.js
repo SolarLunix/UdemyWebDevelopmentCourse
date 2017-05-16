0a384fa2d2e56c3ae3d92b7e6101ff940d1be5e2
Melissa Melaugh <solarlunix@gmail.com>
2017-05-16 10:47:24 +0000

    YelpCamp update

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -8,7 +8,35 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+var campgrounds = [
+    {'name': 'Lazy K Campground',
+        'img': 'http://images.clipartpanda.com/gunfire-clipart-camping-clip-art-5.gif'
+    },
+    {'name': 'New Castle Lake',
+        'img': 'http://watchwrestling.in/wp-content/uploads/2016/05/latest.png'
+    },
+    {'name': 'Brokeback Mountain',
+        'img': 'http://ginazammit.files.wordpress.com/2012/06/camp.jpg'
+    }];
 
+// Get Requests
+app.get('/', function(req, res) {
+   res.render('index'); 
+});
+
+app.get('/campgrounds', function(req, res) {
+    res.render('campgrounds', {campgrounds:campgrounds});
+});
+
+app.get('/campgrounds/new', function(req, res) {
+   res.render('campgroundsNew') ;
+});
+
+// Post requests
+app.post('/campgrounds', function(req, res){
+   
+   res.redirect('/campgrounds'); 
+});
 
 // Invalid URL - Make sure this goes last!
 app.get("*", function(req, res){
@@ -18,5 +46,5 @@ app.get("*", function(req, res){
 
 // Standard listener
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("==== Server Started! ====");
+    console.log("==== YelpCamp Server Started! ====");
 });
